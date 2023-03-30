79756a8864e85a50f2b68af5a595a0923d1f42be
Lucas Medeiros <lucas-medeiros@live.com>
2023-03-29 21:51:50 -0300

    adding errors management

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import 'express-async-errors';
 
 const router = Router();
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import 'express-async-errors';
+import cors from 'cors';
+
 import { router } from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(cors());
+
+app.use((err: Error, req: Request, res: Response) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Iternal server error.'
+  })
+})
+
+
 
 app.listen(3333, () =>  console.log('Servidor online!'))
+
+
 
 
