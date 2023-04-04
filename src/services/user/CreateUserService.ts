091f1079850e2636701bf68e706084e0f681111a
Lucas Medeiros <lucas-medeiros@live.com>
2023-04-04 20:01:39 -0300

    user service

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,16 +1,44 @@
-import { Router, Request, Response } from 'express'
+import prismaClient from "../../prisma";
 
 interface UserRequest {
-  name: string,
-  email: string,
-  password: string
-
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService {
-  async execute({name, email, password}: UserRequest) {
-    return {ok: true}
+  async execute({ name, email, password }: UserRequest) {
+
+    const userAlreadyExists = await prismaClient.findUnique({
+      where: {
+        email: email,
+      },
+    })
+
+    if (!email) {
+      throw new Error("Email incorrect");
+      
+    }
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      },
+    });
+
+    return (user);
   }
 }
 
-export {CreateUserService}
+export { CreateUserService };
